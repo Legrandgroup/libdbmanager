5271d061a02d4477164afdceb835a43256b5ebec
Lionel Ains <lionel.ains@legrand.fr>
2015-01-06 12:55:50 +0100

    Moving DBManagerContainer class out of dbfactory.[ch]pp, into dedicated dbmanagercontainer.[ch]pp files

--- src/dbfactory.cpp
+++ src/dbfactory.cpp
@@ -202,39 +202,3 @@ string DBManagerFactory::locationUrlToPath(string location) {
 	}
 	return databasePath;
 }
-
-DBManagerContainer::DBManagerContainer(string dbLocation, string configurationDescriptionFile):
-		dbLocation(dbLocation),
-		configurationDescriptionFile(configurationDescriptionFile),
-		dbm(DBManagerFactory::getInstance().getDBManager(dbLocation, configurationDescriptionFile)) {
-}
-
-DBManagerContainer::DBManagerContainer(const DBManagerContainer& other):
-		dbLocation(other.dbLocation),
-		configurationDescriptionFile(other.configurationDescriptionFile),
-		dbm(DBManagerFactory::getInstance().getDBManager(dbLocation, configurationDescriptionFile)) {
-}
-
-DBManagerContainer::~DBManagerContainer() {
-	DBManagerFactory::getInstance().freeDBManager(this->dbLocation);
-}
-
-void swap(DBManagerContainer& first, DBManagerContainer& second) /* nothrow */ {
-	using std::swap;	// Enable ADL
-
-	swap(first.dbLocation, second.dbLocation);
-	swap(first.configurationDescriptionFile, second.configurationDescriptionFile);
-
-	/* We whould use swap() here to be exception safe but DBManager does not implement the swap() method */
-	/* Because we are dealing with references, there is no risk of exception during the following 3 lines, which is what we must ensure in this swap() method */
-	DBManager& temp = first.dbm;
-	try {
-		first.dbm = second.dbm;
-		second.dbm = first.dbm;
-	}
-	catch (exception& ex) {
-		cerr << string(__func__) + "(): Error while swapping arguments' dbm attributes\n";
-		throw ex;
-	}
-	/* Once we have swapped the members of the two instances... the two instances have actually been swapped */
-}
